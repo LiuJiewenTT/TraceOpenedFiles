3ddf4556160d222124ddc3440b42aa2a98390c07
刘杰文 <1635370493@qq.com>
2023-11-05 15:49:36 +0800

    fix and add
    - fix -filepath not working.
    - add -persist.
    - support sleeping between checks.
    - update the document.

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 The Python version of lsof. It's for Windows. But it's not designed to be the same as lsof.
 
 It is designed to monitor the file usage of a selected process.
+
+## Parameters Available
+
+| Option | Values | Meaning |
+|:------:|:------:|:-------:|
+| -pid   | [PID integer] | Indicate the target pid. |
+| -file  | string | Choose the filepath of the program to run. |
+| -name  | string | Choose the process to catch by name. |
+| -persist | [Present/Absent] | Determine whether to monitor targets forever. |
+| -sleeplength | float | Set the time length of sleeping between list checks. Set to "preset" will use *0.1* as the value.|
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import difflib
 
 divider = '========================================='
 subdivider = divider.replace('=', '-')
+sleeplength: float
 
 
 def TraceOpenedFiles(PID: int):
@@ -38,46 +39,70 @@ def TraceOpenedFiles(PID: int):
             print('Count of differences: [{}]'.format(diffcnt))
             print(divider)
         l_prev = l
-        # time.sleep(0.1)
+        if sleeplength >= 0:
+            time.sleep(sleeplength)
+
+
+def main():
+    persist = True      # starter
+    global sleeplength
+    while persist is True:
+        pid = None
+        filepath = None
+        name = None
+        persist = False
+        sleeplength = -1
+
+        if sys.argv.__len__() > 1:
+            if '-pid' in sys.argv:
+                pid = int(sys.argv[sys.argv.index('-pid') + 1])
+            if '-file' in sys.argv:
+                filepath = sys.argv[sys.argv.index('-file') + 1]
+            if '-name' in sys.argv:
+                name = sys.argv[sys.argv.index('-name') + 1]
+            if '-persist' in sys.argv:
+                persist = True
+            if '-sleeplength' in sys.argv:
+                sleeplength = sys.argv[sys.argv.index('-sleeplength') + 1]
+                if sleeplength == 'preset':
+                    sleeplength = 0.1
+                else:
+                    sleeplength = float(sleeplength)
+
+        # name = 'IGCC.exe'
+
+        if name is not None:
+            while pid is None:
+                pids = psutil.pids()
+                for i in pids:
+                    try:
+                        p = psutil.Process(i)
+                        if p.name() == name:
+                            pid = i
+                            break
+                    except psutil.NoSuchProcess as e:
+                        pid = None
+                        pass
+
+        if filepath is not None:
+            print('File Path: [{}]'.format(filepath))
+            p = psutil.Popen(filepath)
+            pid = p.pid
+
+        if filepath is None and pid is None and name is None:
+            filepath = input('File path: ')
+            print('File Path: [{}]'.format(filepath))
+            p = psutil.Popen(filepath)
+            # p = psutil.o
+            pid = p.pid
+
+        # pid = eval(input('PID: '))
+        # pid = 20008
+        print(pid)
+        TraceOpenedFiles(pid)
 
 
 if __name__ == '__main__':
-    pid = None
-    filepath = None
-    name = None
-
-    if sys.argv.__len__() > 1:
-        if '-pid' in sys.argv:
-            pid = int(sys.argv[sys.argv.index('-pid')+1])
-        if '-file' in sys.argv:
-            filepath = sys.argv[sys.argv.index('-file')+1]
-        if '-name' in sys.argv:
-            name = sys.argv[sys.argv.index('-name') + 1]
-
-    # name = 'IGCC.exe'
-
-    if name is not None:
-        while pid is None:
-            pids = psutil.pids()
-            for i in pids:
-                try:
-                    p = psutil.Process(i)
-                    if p.name() == name:
-                        pid = i
-                        break
-                except psutil.NoSuchProcess as e:
-                    pass
-
-    if filepath is None and pid is None and name is None:
-        filepath = input('File path: ')
-        print('File Path: [{}]'.format(filepath))
-        p = psutil.Popen(filepath)
-        # p = psutil.o
-        pid = p.pid
-
-    # pid = eval(input('PID: '))
-    # pid = 20008
-    print(pid)
-    TraceOpenedFiles(pid)
+    main()
     pass
 
